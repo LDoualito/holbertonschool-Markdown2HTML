1423a1496cd95e47015d2cf10a7b7edde724b0c8
LDoualito <3242@holbertonschool.com>
2023-04-06 21:07:16 +0200

    start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 """
-markdown2html.py - Convert a markdown file to HTML format
+markdown2html.py - Convert a markdown file to HTML.
 
-Usage: ./markdown2html.py [MARKDOWN_FILE] [HTML_FILE]
+Usage: ./markdown2html.py input_file output_file
+
+Arguments:
+    input_file:  The name of the Markdown file to be converted.
+    output_file: The name of the output HTML file.
+
+If the number of arguments is less than 2, the script will print the usage message and exit with status code 1.
+If the Markdown file does not exist, the script will print an error message and exit with status code 1.
+If the conversion is successful, the script will create the HTML file and exit with status code 0.
 """
 
 import sys
-import os.path
+import os
 import markdown
 
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py input_file output_file", file=sys.stderr)
+    sys.exit(1)
 
-def main():
-    if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py [MARKDOWN_FILE] [HTML_FILE]\n")
-        sys.exit(1)
+input_file = sys.argv[1]
+output_file = sys.argv[2]
 
-    markdown_file = sys.argv[1]
-    html_file = sys.argv[2]
+if not os.path.isfile(input_file):
+    print("Missing file:", input_file, file=sys.stderr)
+    sys.exit(1)
 
-    if not os.path.isfile(markdown_file):
-        sys.stderr.write(f"Missing {markdown_file}\n")
-        sys.exit(1)
+with open(input_file, "r") as f:
+    html = markdown.markdown(f.read())
 
-    with open(markdown_file, "r") as f:
-        markdown_text = f.read()
+with open(output_file, "w") as f:
+    f.write(html)
 
-    html_text = markdown.markdown(markdown_text)
-
-    with open(html_file, "w") as f:
-        f.write(html_text)
-
-    sys.exit(0)
-
-
-if __name__ == "__main__":
-    main()
+sys.exit(0)
