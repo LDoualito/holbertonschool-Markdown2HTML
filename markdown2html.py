71aed6b7292df1ebc906a65bdb2779f9cc72deb0
LDoualito <3242@holbertonschool.com>
2023-04-06 21:20:28 +0200

    task 0 - error documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
-"""
-Script that converts a markdown file to an HTML file.
 
+"""
+Script that converts Markdown to HTML
 Usage: ./markdown2html.py README.md README.html
 """
 
 import sys
-import os
+import os.path
 import markdown
 
 
-def main():
-    # Check that the correct number of arguments are provided
-    if len(sys.argv) != 3:
+if __name__ == "__main__":
+    # Check the number of arguments
+    if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
+    # Get the input and output file names
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
     # Check if the input file exists
-    if not os.path.isfile(sys.argv[1]):
-        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+    if not os.path.isfile(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
 
-    # Read the markdown file
-    with open(sys.argv[1], 'r') as md_file:
-        md_text = md_file.read()
-
-    # Convert the markdown to HTML
-    html_text = markdown.markdown(md_text)
-
-    # Write the HTML to the output file
-    with open(sys.argv[2], 'w') as html_file:
-        html_file.write(html_text)
-
-
-if __name__ == "__main__":
-    main()
+    # Convert the Markdown file to HTML and write it to the output file
+    with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+        html = markdown.markdown(f_in.read())
+        f_out.write(html)
+    
+    sys.exit(0)
