3335da9ee62b351c0f4b0e088fb01bd8ec05a137
LDoualito <3242@holbertonschool.com>
2023-12-10 22:30:27 +0100

    Headings Markdown syntax for generating HTML

--- markdown2html.py
+++ markdown2html.py
@@ -1,60 +1,71 @@
-def convert_markdown_to_html(markdown_file, output_file):
+#!/usr/bin/python3
+"""
+Parse a Markdown file and turns into HTML
+"""
+import sys
+
+
+def unlist(lines):
+    """Gives ul tags to lines and wraps with <ul>"""
+    wrap = ["<ul>\n",]
+
+    for item in lines:
+        text = item.replace('-', '')
+        text.strip()
+        wrap.append(f"<li>{text.strip()}</li>\n")
+
+    wrap.append("</ul>\n")
+    return "".join(wrap)
+
+
+def headings(lines):
+    """Gives heading tags to lines"""
+    wrap = []
+
+    for item in lines:
+        lvl = item.count('#')
+        text = item.replace('#', '')
+        wrap.append(f"<h{lvl}>{text.strip()}</h{lvl}>\n")
+
+    return "".join(wrap)
+
+
+def read_file(src, dest):
     """
-    Converts Markdown to HTML.
+    Parse Markdown and save as HTML.
+
+    Each line is gonna be wrapped by its own specific tag function
+    and returned as a string saved on @render list, then each item
+    on render is gonna be written in @dest as a string.
 
     Args:
-        markdown_file (str): Name of the Markdown file.
-        output_file (str): Name of the output HTML file.
+        src (str): Source file.
+        dest (str): Destination of source file.
     """
-    # Check if the Markdown file exists
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    fun_dic = {'#': headings, '-': unlist}
+
+    render = []
+
+    try:
+        with open(src, 'r', encoding='utf-8') as file:
+            content = file.read()
+    except FileNotFoundError:
+        sys.stderr.write(f"Missing {src}\n")
         sys.exit(1)
 
-    # Read the Markdown file and process headings
-    with open(markdown_file, 'r') as f:
-        lines = f.readlines()
+    for mark in fun_dic:
+        aux = [line for line in content.split('\n') if mark in line]
+        render.append(fun_dic.get(mark)(aux))
 
-    # Process each line
-    output_lines = []
-    for line in lines:
-        line = line.rstrip('\n')
+    with open(dest, 'w', encoding='utf-8') as to_html:
+        for line in render:
+            to_html.write("".join(line))
 
-        # Check for heading syntax and convert to HTML
-        if line.startswith('# '):
-            output_lines.append(f"<h1>{line[2:]}</h1>")
-        elif line.startswith('## '):
-            output_lines.append(f"<h2>{line[3:]}</h2>")
-        elif line.startswith('### '):
-            output_lines.append(f"<h3>{line[4:]}</h3>")
-        elif line.startswith('#### '):
-            output_lines.append(f"<h4>{line[5:]}</h4>")
-        elif line.startswith('##### '):
-            output_lines.append(f"<h5>{line[6:]}</h5>")
-        elif line.startswith('###### '):
-            output_lines.append(f"<h6>{line[7:]}</h6>")
-        else:
-            output_lines.append(line)
-
-    # Write the HTML output
-    with open(output_file, 'w') as output:
-        output.write('\n'.join(output_lines))
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
         sys.exit(1)
 
-    # Extracting the filename without extension from sys.argv[2]
-    fn_no_extension = os.path.splitext(sys.argv[2])[0]
-
-    # Using the correct HTML filename when opening for reading
-    fn_html = f"{fn_no_extension}.html"
-
-    try:
-        with open(fn_html, "r") as f:
-            pass
-    except FileNotFoundError:
-        print("FileNotFoundError: [Errno 2] No such file or directory:", repr(fn_html))
-        sys.exit(1)
+    read_file(sys.argv[1], sys.argv[2])
     sys.exit(0)
